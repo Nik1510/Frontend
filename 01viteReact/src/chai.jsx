53d64b33e5bab598e8ef8e882af966196877336f
Nikhil Shaw <Nikhilshaw575@gmail.com>
2025-05-22 17:33:36 +0530

    feat: update App component and remove unused styles; add custom rendering logic

---
+++ 01viteReact/src/chai.jsx
@@ -0,0 +1,7 @@
+function Chai(){
+    return(
+        <h3> Nikhil</h3>
+    )
+}
+
+export default Chai
--- 01viteReact/src/main.jsx
+++ 01viteReact/src/main.jsx
@@ -1,10 +1,34 @@
 import { StrictMode } from 'react'
+import React from 'react';
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+function Myapp(){
+  return(
+    <div>
+      <h1>Custom App</h1>
+    </div>
+  )
+}
+
+const anotherUser = "Chai aur React"
+
+const reactElement =React.createElement(
+  'a',
+  {
+    href:"https://google.com",
+    target:"_blank"
+  },
+  "click me to visit google",
+  anotherUser
+)
+
+const anotherElement =(
+  <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+
+createRoot(document.querySelector('#root')).render(
+  // <App/>
+  reactElement
 )
